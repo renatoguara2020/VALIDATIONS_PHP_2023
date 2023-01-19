6e57a38576ad79f64507f038c019975bf3085c88
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-19 19:28:36 -0300

    ok

--- SELECT.php
+++ SELECT.php
@@ -9,12 +9,14 @@ try{
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  $stmt = $conn->prepare("SELECT id, firstname, lastname FROM MyGuests");
+  $stmt = $conn->prepare("SELECT * FROM usuarios");
   $stmt->execute();
 
   while($row = $stmt->fetchAll(PDO::FETCH_ASSOC)){
 
-    echo  $row['id'] .' '. $row['firstname']. ' ' . $row['lastname'] .' '.$row['email'];
+    echo '<pre>';
+      print_r($row);
+    echo '</pre>';  
   }
 
 }catch(PDOException $e){
--- insert_into.php
+++ insert_into.php
@@ -1,22 +1,29 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDBPDO";
+$username = "root";
+$password = "";
+$dbname = "testes_pdo";
+
+
+$firstname = 'NATHAN BARBOSA SOARES';
+$lastname = 'BARBOSA SOARES';
+$email = 'nathan2020@gmail.com';
+
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
+  $stmt = $conn->prepare("INSERT INTO usuarios (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
   // use exec() because no results are returned
   $stmt->bindParam(':firstname', $firstname, PDO::PARAM_STR);
   $stmt->bindParam(':lastname', $lastname, PDO::PARAM_STR);
   $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+  $stmt->execute();
   echo "New record created successfully";
 } catch(PDOException $e) {
-  echo $sql . "<br>" . $e->getMessage();
+  echo $sql . "<br>" . $e->getMessage() . "<br>" . $e->getTraceAsString() ;
 }
 
 $conn = null;
