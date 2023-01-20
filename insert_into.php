2b4f0388fe0300aee1b1988cdfd6c8cbd706b54c
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-20 08:52:03 -0300

    ok

--- insert_into.php
+++ insert_into.php
@@ -2,12 +2,12 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "testes_pdo";
+$dbname = "mydatabase";
 
 $firstname = (isset($_POST["firstname"]) && $_POST["firstname"] != null) ? $_POST["firstname"] : "";
 $lastname = (isset($_POST["lastname"]) && $_POST["lastname"] != null) ? $_POST["lastname"] : "";
-$email = (isset($_POST["email"]) && $_POST["email"] != null) ? $_POST["email"] : "";
-$estado = (isset($_POST["estado"]) && $_POST["estado"] != null) ? $_POST["estado"] : "";
+$username = (isset($_POST["username"]) && $_POST["username"] != null) ? $_POST["username"] : "";
+$estados = (isset($_POST["estados"]) && $_POST["estados"] != null) ? $_POST["estados"] : "";
 $cidade = (isset($_POST["cidade"]) && $_POST["cidade"] != null) ? $_POST["cidade"] : "";
 $cep = (isset($_POST["cep"]) && $_POST["cep"] != null) ? $_POST["cep"] : "";
 
@@ -16,11 +16,15 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  $stmt = $conn->prepare("INSERT INTO usuarios (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
+  $stmt = $conn->prepare("INSERT INTO usuarios (firstname, lastname, estados, cep, username, cidade) 
+  VALUES (:firstname, :lastname, :estados, :cep, :username, :cidade)");
   // use exec() because no results are returned
   $stmt->bindParam(':firstname', $firstname, PDO::PARAM_STR);
   $stmt->bindParam(':lastname', $lastname, PDO::PARAM_STR);
-  $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+  $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+  $estados->bindParam(':estados', $estados, PDO::PARAM_STR);
+  $cep->bindParam(':cep', $cep, PDO::PARAM_STR);
+  $username->bindParam(':cidade', $cidade, PDO::PARAM_STR);
   $stmt->execute();
 
   if($stmt->rowCount() > 0){
