63be7a5547f7327a47c890ab5c765cd37f405e78
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-19 23:25:01 -0300

    OK

--- selectPDO_ORDER_BY.php
+++ selectPDO_ORDER_BY.php
@@ -8,6 +8,7 @@ $dbname = "myDBPDO";
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   $stmt = $conn->prepare("SELECT id, firstname, lastname FROM usuarios ORDER BY id");
   $stmt->execute();
 
